9a356cd09cdb7ede41e9687b4dd933ec29cd4f81
Raiyan <raiyan.sharif.1234@gmail.com>
2020-08-07 20:11:09 +0600

    Updates

---
+++ precache-manifest.7448439a1de15b6e789920363c971a8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630521854f31c4b8690b05e2fc4d60e1",
+    "url": "/covid-19-crf/index.html"
+  },
+  {
+    "revision": "5930fc54a076b37ead7c",
+    "url": "/covid-19-crf/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cf7e6479239828a64d4",
+    "url": "/covid-19-crf/static/js/2.ac13903f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-crf/static/js/2.ac13903f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5930fc54a076b37ead7c",
+    "url": "/covid-19-crf/static/js/main.fdb73f8c.chunk.js"
+  },
+  {
+    "revision": "7d63929f468d90f09954",
+    "url": "/covid-19-crf/static/js/runtime-main.7aed70dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-crf/static/media/logo.5d5d9eef.svg"
+  }
+]);
